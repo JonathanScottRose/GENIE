bd955d824008e75d3bb4631f9b30b23cea6585fb
Alex Rodionov <arod@eecg.toronto.edu>
2017-12-06 16:47:57 -0500

    updated pipe_stage with WYSIWYG

--- data/genie_pipe_stage.sv
+++ data/genie_pipe_stage.sv
@@ -14,28 +14,22 @@ module genie_pipe_stage #
 	output logic o_valid,
 	input i_ready
 );
-
 	logic [WIDTH-1:0] data0, data1;
 	logic valid0, valid1;
 	logic ready0;
 	
-	wire xfer0 = valid0 && i_ready;
-	wire xfer1 = valid1 && ready0;
+	wire xfer = !valid0 || i_ready;
 	
 	always_ff @ (posedge i_clk or posedge i_reset) begin
 		if (i_reset) begin
-			valid0 <= '0;
-			valid1 <= '0;
 			ready0 <= '1;
-			data0 <= 'x;
-			data1 <= 'x;
+			valid0 <= '0;
 		end
 		else begin
-			ready0 <= !valid0 || i_ready;
-			
-			if (!valid0 || i_ready) begin
+			ready0 <= xfer;
+		
+			if (xfer) begin
 				valid0 <= ready0? i_valid : valid1;
-				data0 <= ready0? i_data : data1;
 			end
 			
 			if (ready0) begin
@@ -44,6 +38,35 @@ module genie_pipe_stage #
 			end
 		end
 	end
+	
+	// synthesis translate_off
+	always_ff @ (posedge i_clk) begin
+		if (xfer) begin
+			data0 <= ready0? i_data : data1;
+		end
+	end
+	// synthesis translate_on
+	
+	// synthesis read_comments_as_HDL on
+	// genvar i;
+	// generate
+	// for (i = 0; i < WIDTH; i++) begin : data_regs
+	//     dffeas data0_reg
+	//     (
+	//         .d(i_data[i]),
+	//         .clk(i_clk),
+	//         .clrn(~i_reset),
+	//         .ena(xfer),
+	//         .asdata(data1[i]),
+	//         .sload(ready0),
+	//         .q(data0[i]),
+	//         .prn(1'b1),
+	//         .aload(1'b0),
+	//         .sclr(1'b0)
+	//     );
+	// end
+	// endgenerate
+	// synthesis read_comments_as_HDL off
 	
 	assign o_data = data0;
 	assign o_valid = valid0;
